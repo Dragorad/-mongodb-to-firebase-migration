33c7f095caaa91d6ac306830b24b3a98d6cd0de1
Dragorad <mi.e.drago@gmail.com>
2025-03-13 17:36:12 +0200

    emulators init

--- functions/index.js
+++ functions/index.js
@@ -1,44 +1,51 @@
-import { https } from 'firebase-functions';
-import { db } from '../src/firebaseInit.js'; // Импортиране на Firestore
-import { collection, query, where, orderBy, limit, getDocs } from 'firebase/firestore';
+import {https} from "firebase-functions";
+import {db} from "./firebaseInit.js";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  limit,
+  getDocs,
+} from "firebase/firestore";
 
 export const queryTitleRecords = https.onRequest(async (req, res) => {
   try {
     const queryObj = req.body;
-    const { bpm, delta, average_loudness, spectral_centroid, genresArr } = queryObj;
+    const {bpm, delta, averageLoudness, spectralCentroid, genresArr} = queryObj;
 
     const deltaMod = delta * 0.1;
-    const titleRecords = collection(db, 'title_records');
+    const titleRecords = collection(db, "title_records");
 
-    let bpmMin = bpm - bpm * deltaMod;
-    let bpmMax = bpm + bpm * deltaMod;
-    let loudnessMin = 0.0644 + average_loudness / 100 - deltaMod;
-    let loudnessMax = 0.0644 + average_loudness / 100 + deltaMod;
+    const bpmMin = bpm - bpm * deltaMod;
+    const bpmMax = bpm + bpm * deltaMod;
+    const loudnessMin = 0.0644 + averageLoudness / 100 - deltaMod;
+    const loudnessMax = 0.0644 + averageLoudness / 100 + deltaMod;
 
-    let spectralNum = 200 + spectral_centroid * 21;
-    let spectralMin = spectralNum - delta * 21;
-    let spectralMax = spectralNum + delta * 21;
+    const spectralNum = 200 + spectralCentroid * 21;
+    const spectralMin = spectralNum - delta * 21;
+    const spectralMax = spectralNum + delta * 21;
 
     const q = query(
-      titleRecords,
-      where('genres', 'array-contains-any', genresArr),
-      where('bpm', '>=', bpmMin),
-      where('bpm', '<=', bpmMax),
-      where('lowLevelSpectral.average_loudness', '>=', loudnessMin),
-      where('lowLevelSpectral.average_loudness', '<=', loudnessMax),
-      where('lowLevelSpectral.spectral_centroid.mean', '>=', spectralMin),
-      where('lowLevelSpectral.spectral_centroid.mean', '<=', spectralMax),
-      where('url', '!=', null),
-      orderBy('chords_key'),
-      orderBy('bpm'),
-      limit(10)
+        titleRecords,
+        where("genres", "array-contains-any", genresArr),
+        where("bpm", ">=", bpmMin),
+        where("bpm", "<=", bpmMax),
+        where("lowLevelSpectral.averageLoudness", ">=", loudnessMin),
+        where("lowLevelSpectral.averageLoudness", "<=", loudnessMax),
+        where("lowLevelSpectral.spectralCentroid.mean", ">=", spectralMin),
+        where("lowLevelSpectral.spectralCentroid.mean", "<=", spectralMax),
+        where("url", "!=", null),
+        orderBy("chords_key"),
+        orderBy("bpm"),
+        limit(10),
     );
 
     const snapshot = await getDocs(q);
-    const results = snapshot.docs.map(doc => doc.data());
+    const results = snapshot.docs.map((doc) => doc.data());
     res.json(results);
   } catch (error) {
-    console.error('Error querying title records:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error querying title records:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
